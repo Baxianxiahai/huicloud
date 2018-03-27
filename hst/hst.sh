25423de11fb9b97fd44fdaa902e1f76075f88979
hitpony <smdzjl@sina.cn>
2018-03-27 20:28:31 +0800

    zjl@2018/3/27

--- hst/hst.sh
+++ hst/hst.sh
@@ -24,18 +24,39 @@ set -x
 #Define command
 #在不同的服务器中，python3.6的安装地址可能不一样，将会导致这个绝对路径不一样，从而导致本脚本不一样，需要小心注意！
 #
-cmdExec = "/opt/pycv/bin/python3.6 /var/www/html/huicloud/hst/hstMain.py"
+
+if [ -e /opt/pycv/bin/python3 ]
+then
+	export PATH=/opt/pycv/bin:$PATH
+	export LD_LIBRARY_PATH=/opt/pycv/lib:$LD_LIBRARY_PATH
+	export PYTHON_INCLUDE_DIRS=/opt/pycv/include/python3.6dm
+	export PYTHON_LIBRARIES=/opt/pycv/lib/libpython3.6dm.a
+	pyExec=/opt/pycv/bin/python3
+elif [ -e /usr/local/bin/python3 ]
+then
+	export PATH=/usr/local/bin:$PATH
+        export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+        export PYTHON_INCLUDE_DIRS=/usr/local/include/python3.6dm
+        export PYTHON_LIBRARIES=/usr/local/lib/libpython3.6dm.a
+	pyExec=/usr/local/bin/python3
+else
+	pyExec=/usr/bin/python3
+fi
+
+
+
+cmdExec="${pyExec} /var/www/html/huicloud/hst/hstMain.py"
 
 do_start()
 {
 	#判定hst是否运行
-	if pgrep $cmdExec > /dev/null  ; then
+	if pgrep -f hstMain.py > /dev/null  ; then
 		echo "hst is running"
 		exit 1
 	else
 		echo "hst not running" 
-		sudo cd /
-		sudo -S /opt/pycv/bin/python3.6 /var/www/html/huicloud/hst/hstMain.py &
+		cd /
+		${pyExec} /var/www/html/huicloud/hst/hstMain.py &
 		sleep 1
 	fi
 }
@@ -43,14 +64,14 @@ do_start()
 #
 # Function that starts the daemon/service
 #
-HST_PID = ""
+HST_PID=""
 
 do_stop()
 {
-	if pgrep $cmdExec > /dev/null  ; then
+	if pgrep -f hstMain.py > /dev/null  ; then
 		echo "hst is running ..."
-		HST_PID = $(pidof $cmdExec)
-		kill -9 $(pidof $cmdExec) 
+		HST_PID = $(pgrep -f hstMain.py)
+		kill -9 $(pgrep -f hstMain.py) 
 		echo "hst killed ..."
 		exit 1
 	else
